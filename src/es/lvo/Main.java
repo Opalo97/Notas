60a1b7f03ebf80284cb01ba3d568804668fbf515
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-02-09 09:27:15 +0100

    terminado

--- src/es/lvo/Main.java
+++ src/es/lvo/Main.java
@@ -5,38 +5,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int numero,nota = 0,media;
+        int numeroDeNotas, aprobados = 0, suspensos = 0;
+        float nota, media, suma = 0f;
         Scanner entrada = new Scanner(System.in);
 
         System.out.print("¿Cuántas notas quieres introducir? ");
-        numero = entrada.nextInt();
+        numeroDeNotas = entrada.nextInt();
 
-        for (int i = 0; i < numero; i++) {
+        for (int i = 0; i < numeroDeNotas; i++) {
             do {
-                System.out.print("Nota " + (i + 1) + ":");
+                System.out.print("Nota " + (i + 1) + ": ");
                 nota = entrada.nextInt();
             } while (nota < 0 || nota > 10);
-            System.out.println();
+
+            suma += nota;
+
+            if (nota >= 5) {
+                aprobados++;
+            } else {
+                suspensos++;
+            }
         }
 
-        while (nota > 5) {
-            int i = 0;
-            i = 1;
-            i++;
-        }System.out.println("Número de aprobados: " );
+        System.out.println();
 
+        System.out.println("Número de aprbados: " + aprobados);
+        System.out.println("Número de suspensos: " + suspensos);
 
-        for (int i = 0; nota > 5; i++){
-        }System.out.println("Número de aprobados: " + nota);
-
-        for (int i = 0; nota < 5; i++){
-             nota = i;
-        }System.out.println("Número de suspensos: " + nota);
-
-        media = nota;
+        media = suma / (float)numeroDeNotas;
         System.out.println("Media de notas: "+ media);
 
-
-
+        entrada.close();
     }
 }
